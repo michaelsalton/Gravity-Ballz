f5b183af8562fa12d6fd94c93f5229e4fc983517
Michael Salton <msalton@uwo.ca>
2023-04-26 16:50:08 -0400

    fixed bounce

---
+++ Ball.cpp
@@ -0,0 +1,41 @@
+#include <math.h>
+
+class Ball {
+private:
+    int x, y; // Position of the ball
+    int xVelocity, yVelocity; // Speed of the ball
+    int radius; // Radius of the ball
+    // Add any other necessary private members here
+
+public:
+    // Constructor
+    Ball(int startX, int startY, int startSpeedX, int startSpeedY, int startRadius) {
+        x = startX;
+        y = startY;
+        xVelocity = startSpeedX;
+        yVelocity = startSpeedY;
+        radius = startRadius;
+    }
+
+    // Move the ball by updating its position based on its speed
+    void move() {
+        x += xVelocity;
+        y += yVelocity;
+    }
+
+    // Draw the ball on the screen using a graphics library
+    void draw() {
+        // Add code here to draw the ball using a graphics library
+    }
+
+    // Check for collisions with another ball
+    bool checkCollision(Ball otherBall) {
+        int distance = sqrt(pow(x - otherBall.x, 2) + pow(y - otherBall.y, 2));
+        if (distance <= radius + otherBall.radius) {
+            return true;
+        }
+        return false;
+    }
+
+    // Add any other necessary public members here
+};
--- Main.cpp
+++ Main.cpp
@@ -4,16 +4,19 @@
 // Normal stuff
 #include <chrono>
 #include <thread>
+#include <math.h>
+#include <iostream>
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1200, 900), "Gravity Ballz ++");
     sf::CircleShape shape(100.f);
     float x = 400;
-    float y = 000;
+    float y = 0;
     float dx = 0; // x velocity
     float dy = 0; // y velocity
     float g = 1; // dy^2
+    bool isMoving = true;
     
     shape.setFillColor(sf::Color::Green);
 
@@ -26,16 +29,29 @@ int main()
                 window.close();
         }
 
-        // Update position
-        x += dx;
-        y += dy;
         dy += g;
+        if (isMoving){
+            // Update position
+            x += dx;
+            y += dy;
+            
+            // Check boundaries and reverse direction if necessary
+            if (x + 2 * shape.getRadius() > window.getSize().x || x < 0)
+                dx = -dx;
+            if (y + 2 * shape.getRadius() > window.getSize().y) {
+                dy = -0.7 * dy; // reduce magnitude of y-velocity
+                y = window.getSize().y - 2 * shape.getRadius(); // prevent ball from going below the bottom of the window
+            }
+
+            std::cout << dy << std::endl;
+
+            // Stop ball if velocity is very small
+            if (abs(dy) <= 0.1) {
+                isMoving = false;
+            }
+        }
+
         
-        // Check boundaries and reverse direction if necessary
-        if (x + 2 * shape.getRadius() > window.getSize().x || x < 0)
-            dx = -dx;
-        if (y + 2 * shape.getRadius() > window.getSize().y || y < 0)
-            dy = -0.7*dy;
 
         // Draw circle and display window
         window.clear();
